a4bb4815d06479c78ea5006f996ec4b8d79c704c
Reuben Germain <reuben.germain@soprasteria.com>
2023-05-13 11:36:51 +0100

    challenge_one

---
+++ challenge_one.py
@@ -0,0 +1,35 @@
+def string_fun(string):
+    '''
+    Return a tuple with three elements.
+    The first element is True if the string contains only alphabet characters, otherwise False.
+    The second element is True if the string ends with an exclamation mark ('!'), otherwise False.
+    The third element is the string with all spaces (' ') replaced with hyphens ('-').
+    Arguments
+    string: a string
+    Examples
+    string_fun('Hello World!') returns (False, True, 'Hello-World!')
+    string_fun('ThisIsAChallenge') returns (True, False, 'ThisIsAChallenge')
+    '''
+
+    # ====================================
+    # Do not change the code before this
+
+    # CODE1: Write code that will assign details with the appropriate tuple
+    details = "Reuben 123"
+    lname = "Germain!"
+    pet = "My dog is not feeling well".replace("", "-")
+    return details, lname, pet
+
+    result = string_fun(string)
+
+    print(result)
+
+    # ====================================
+    # Do not change the code after this
+
+    return details
+
+
+if __name__ == '__main__':
+    print(string_fun('Hello World!'))
+    print(string_fun('ThisIsAChallenge'))
